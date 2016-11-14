b6c84c71725601f15288e6acceda611bd417e2f5
Vandeportaele Bertrand <bvandepo@laas.fr>
2016-11-14 12:33:24 +0100

    added empty directories creation in the script

--- getData.sh
+++ getData.sh
@@ -3,3 +3,9 @@
 wget http://homepages.laas.fr/bvandepo/files/autoCornerTagFinder_linux/inputImages.zip
 unzip inputImages.zip
 rm inputImages.zip
+
+#create empty directories
+mkdir -p cToMatlab
+mkdir -p outputImages
+mkdir -p timer
+
